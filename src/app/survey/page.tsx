dd3c544e15822258d7c48003b6fbf7ce915f5526
Mariam4324 <ashkmariam@gmail.com>
2024-11-07 17:22:11 +0300

    fix: surveys validation setup

--- src/app/survey/page.tsx
+++ src/app/survey/page.tsx
@@ -25,6 +25,7 @@ const questions = [
 
 const page = () => {
   const [currentQuestionId, setCurrentQuestionId] = useState(1);
+  const [selectedOption, setSelectedOption] = useState<boolean>(false);
   let currentQue = questions.find((que) => que.id === currentQuestionId);
 
   useEffect(() => {
@@ -35,13 +36,12 @@ const page = () => {
     } else {
       currentQue = { id: 0, question: "error", options: ["please, try later"] };
     }
-
-    console.log(currentQue);
   }, [currentQuestionId]);
 
   const scrollPageHandler = () => {
     if (currentQuestionId <= questions.length) {
       return setCurrentQuestionId((prev) => prev + 1);
+      setSelectedOption(false)
     }
   };
 
@@ -74,12 +74,12 @@ const page = () => {
         </>
       ) : (
         <>
-          <Question queTitle={currentQue.question} queAnswers={currentQue.options} />
+          <Question setSelectedOption={setSelectedOption} queTitle={currentQue.question} queAnswers={currentQue.options} />
           <div className={css.survey__btns}>
             <Button className={css["survey-btn"]} onClick={scrollPageBackHandler} variant="outlined" disabled={currentQuestionId === 1}>
               Back
             </Button>
-            <Button className={css["survey-btn"]} onClick={scrollPageHandler} variant="outlined" disabled={currentQuestionId > questions.length}>
+            <Button className={css["survey-btn"]} onClick={scrollPageHandler} variant="outlined" disabled={currentQuestionId > questions.length || selectedOption === false}>
               Next
             </Button>
           </div>
--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -1,13 +1,19 @@
 import { FormControl, RadioGroup, Typography } from "@mui/material";
 import SurveyRadio from "../SurverRadio/SurveyRadio";
 import css from "./Question.module.scss";
+import { Dispatch, SetStateAction } from "react";
 
 interface QuestionProps {
   queTitle: string;
   queAnswers: string[];
+  setSelectedOption: Dispatch<SetStateAction<boolean>>;
 }
 
-const Question = ({ queTitle, queAnswers }: QuestionProps) => {
+const Question = ({ queTitle, queAnswers, setSelectedOption }: QuestionProps) => {
+  const radioSelectHandler = () => {
+    setSelectedOption(true);
+  };
+
   return (
     <section className={css.que}>
       <div className={css.que__holder}>
@@ -15,7 +21,7 @@ const Question = ({ queTitle, queAnswers }: QuestionProps) => {
           {queTitle || "Question does not exist"}
         </Typography>
         <FormControl className={css.que__options}>
-          <RadioGroup row aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group">
+          <RadioGroup onClick={radioSelectHandler} row aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group">
             {queAnswers.length
               ? [...queAnswers].map((el) => {
                   return <SurveyRadio key={el} value={el} />;
