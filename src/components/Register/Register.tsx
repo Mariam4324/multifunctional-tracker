1d52ce3c9e3f1d7d29176c6553d603d01ec013a9
Mariam4324 <ashkmariam@gmail.com>
2024-11-10 14:11:53 +0300

    zod setup

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -3,6 +3,8 @@ import { Button, TextField } from "@mui/material";
 import ArrowCircleRightRoundedIcon from "@mui/icons-material/ArrowCircleRightRounded";
 import css from "./Register.module.scss";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 type FormField = {
   name: string;
@@ -10,13 +12,22 @@ type FormField = {
   password: string;
 };
 
-const page = () => {
+// Валидация с использованием Zod
+const schema = z.object({
+  name: z.string().min(2, "Name must be at least 2 characters long").max(40, "Name must be less than 40 characters"),
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(8, "Password must have at least 8 characters").max(25, "Password must be less than 25 characters"),
+});
+
+const Page = () => {
   const {
     register,
     handleSubmit,
     setError,
     formState: { errors, isSubmitting },
-  } = useForm<FormField>();
+  } = useForm<FormField>({
+    resolver: zodResolver(schema),
+  });
 
   const onSubmit: SubmitHandler<FormField> = async (data) => {
     try {
@@ -24,8 +35,8 @@ const page = () => {
       console.log(data);
     } catch (error) {
       setError("email", {
-        message: "this email is already taken"
-      })
+        message: "This email is already taken",
+      });
     }
   };
 
@@ -41,12 +52,21 @@ const page = () => {
 
         <div className={css["main-form__holder"]}>
           <TextField
-            {...(register("name"),
-            {
-              required: "name is required",
-              minLength: 2,
-              maxLength: 60,
-            })}
+            {...register(
+              "name",
+
+              {
+                required: "Name is required",
+                minLength: {
+                  value: 2,
+                  message: "Name must have at least 2 chars",
+                },
+                maxLength: {
+                  value: 40,
+                  message: "Name can not be more than 40 chars",
+                },
+              }
+            )}
             className="white-text-field"
             style={{
               color: "#fff",
@@ -61,16 +81,13 @@ const page = () => {
           {errors.name && <p className="error-text">{errors.name.message}</p>}
 
           <TextField
-            {...(register("email"),
-            {
-              required: "email is required",
-              validate: (value) => {
-                if (!value.includes("@")) {
-                  return "email must include @";
-                }
-                return true;
-              },
-            })}
+            {...register(
+              "email",
+
+              {
+                required: "email is required",
+              }
+            )}
             className="white-text-field"
             style={{
               color: "#fff",
@@ -79,18 +96,21 @@ const page = () => {
             }}
             id="outlined-email-input"
             label="Email"
-            type="Email"
+            type="email"
             placeholder="Enter email"
           />
           {errors.email && <p className="error-text">{errors.email.message}</p>}
 
           <TextField
-            {...(register("password"),
-            {
+            {...register("password", {
               required: "password is required",
               minLength: {
                 value: 8,
-                message: "password must have at least 8 chars",
+                message: "password must be at least 8 chars",
+              },
+              maxLength: {
+                value: 25,
+                message: "password can not be more than 25 chars",
               },
             })}
             className="white-text-field"
@@ -110,8 +130,8 @@ const page = () => {
 
         <Button
           className={css["main-form__btn"]}
-          disabled={isSubmitting ? "Loading..." : "Lets go"}
-          // href="/survey"
+          disabled={isSubmitting}
+          href="/survey"
           type="submit"
           style={{
             width: "100%",
@@ -123,11 +143,11 @@ const page = () => {
           variant="outlined"
           endIcon={<ArrowCircleRightRoundedIcon />}
         >
-          Let's go
+          {isSubmitting ? "Loading..." : "Let's go"}
         </Button>
       </form>
     </section>
   );
 };
 
-export default page;
+export default Page;
