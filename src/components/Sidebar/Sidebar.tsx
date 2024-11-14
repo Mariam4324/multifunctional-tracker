e96225d199e59ac176704ea9d73d2d42dbdab804
Mariam4324 <ashkmariam@gmail.com>
2024-11-14 19:13:20 +0300

    feat: lucide setup

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,19 +1,18 @@
 import css from "./Sidebar.module.scss";
-import CottageIcon from "@mui/icons-material/Cottage";
-import TaskIcon from "@mui/icons-material/Task";
 import { Button } from "@mui/material";
 import Profile from "../Avatar/Avatar";
+import { House, LayoutList } from "lucide";
 
 const sidebarLinks = [
   {
     title: "Home",
-    path: "/",
-    icon: CottageIcon,
+    path: "/dashboard/home",
+    icon: House,
   },
   {
     title: "Tasks",
     path: "/dashboard/tasks",
-    icon: TaskIcon,
+    icon: LayoutList,
   },
 ];
 
@@ -24,22 +23,27 @@ const Sidebar = () => {
 
       <nav className={css.sidebar__menu}>
         <ul className={css.sidebar__list}>
-          {sidebarLinks.map((el) => (
-            <Button
-              href={el.path}
-              style={{
-                display: "flex",
-                justifyContent: "start",
-                textTransform: "none",
-                width: "100%",
-              }}
-              key={el.title}
-              variant="text"
-              startIcon={<el.icon />}
-            >
-              {el.title}
-            </Button>
-          ))}
+          {sidebarLinks.map((el) => {
+            return (
+              <>
+                <Button
+                  href={el.path}
+                  style={{
+                    display: "flex",
+                    justifyContent: "start",
+                    alignItems: "center",
+                    textTransform: "none",
+                    width: "100%",
+                    color: "#fff",
+                  }}
+                  key={el.title}
+                  variant="text"
+                >
+                  {el.title}
+                </Button>
+              </>
+            );
+          })}
         </ul>
       </nav>
     </aside>
