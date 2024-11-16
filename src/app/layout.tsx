6f0707411856f2a335d63e38d35058bb37662bfd
Mariam4324 <ashkmariam@gmail.com>
2024-11-16 17:38:20 +0300

    fealt: projects name changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,19 +7,22 @@ interface LayoutProps {
   children: ReactNode;
 }
 
-const poppinsFont = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800", "900"],
-});
+const poppinsFont = Poppins({ subsets: ["latin"], weight: ["400", "500", "600", "700", "800", "900"] });
 
 export const metadata: Metadata = {
-  title: "LifeTrack Pro",
+  title: "Tlo",
   description: "Your Ultimate Wellness Dashboard",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/x-icon" href="/favicon.png"></link>
+      </head>
       <body className={poppinsFont.className}>{children}</body>
     </html>
   );
