606e7b6978ea3303e7ff3aefcf9b162c1095aece
Mariam4324 <ashkmariam@gmail.com>
2024-11-20 21:17:43 +0300

    feat: budget component

--- src/components/Budget/Budget.tsx
+++ src/components/Budget/Budget.tsx
@@ -1,10 +1,20 @@
-import Card from "../Card/Card";
+"use client";
+import { Button } from "@mui/material";
 import css from "./Budget.module.scss";
+import { CirclePlus } from "lucide-react";
+import { useState } from "react";
+import CreateBudget from "../CreateBudget/CreateBudget";
 
 const Budget = () => {
+  const [budgetModal, setBudgetModal] = useState(false);
+
   return (
     <div className={css.budget}>
-      <Card backgroundColor="primary">add budget</Card>
+      {budgetModal ? <CreateBudget /> : ""}
+      <Button onClick={() => setBudgetModal(true)} className={css.budget__add} variant="text">
+        <CirclePlus />
+        Create new budget
+      </Button>
     </div>
   );
 };
---
+++ src/components/CreateBudget/CreateBudget.tsx
@@ -0,0 +1,5 @@
+const CreateBudget = () => {
+  return <div>CreateBudget comp</div>;
+};
+
+export default CreateBudget;
