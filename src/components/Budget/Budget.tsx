bba0ac6dcdc6afdcb26ed6a875bbb1ce65f8cfd4
Mariam4324 <ashkmariam@gmail.com>
2024-12-19 20:07:28 +0300

    fix: shadcn ui and tailwind css deletead

--- src/components/Budget/Budget.tsx
+++ src/components/Budget/Budget.tsx
@@ -1,51 +1,22 @@
 "use client";
 import { Button } from "@mui/material";
-// import css from "./Budget.module.scss";
-// import { CirclePlus } from "lucide-react";
-// import { useState } from "react";
-// import CreateBudget from "../CreateBudget/CreateBudget";
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
-
-
+import css from "./Budget.module.scss";
+import { CirclePlus } from "lucide-react";
+import { useState } from "react";
+import CreateBudget from "../CreateBudget/CreateBudget";
 
 const Budget = () => {
+  const [budgetModal, setBudgetModal] = useState(false);
   return (
-    // <div className={css.budget}>
-    //   {budgetModal ? <CreateBudget /> : ""}
-    //   <Button onClick={() => setBudgetModal(true)} className={css.budget__add} variant="text">
-    //     <CirclePlus />
-    //     Create new budget
-    //   </Button>
-    // </div>
+    <div onClick={() => setBudgetModal(true)} className={css.budget}>
+      <Button  className={css.budget__add} variant="text">
+        <CirclePlus />
+        Create new budget
+      </Button>
 
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">Edit Profile</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>Make changes to your profile here. Click save when you're done.</DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <label htmlFor="name" className="text-right">
-              Name
-            </label>
-            <input id="name" value="Pedro Duarte" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <label htmlFor="username" className="text-right">
-              Username
-            </label>
-            <input id="username" value="@peduarte" className="col-span-3" />
-          </div>
-        </div>
-        <DialogFooter>
-          <button type="submit">Save changes</button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+      {budgetModal ? <CreateBudget /> : ""}
+    </div>
+          
   );
 };
 
--- src/components/CreateBudget/CreateBudget.tsx
+++ src/components/CreateBudget/CreateBudget.tsx
@@ -1,11 +1,42 @@
 // import EmojiPicker from "emoji-picker-react";
+import { FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 import css from "./CreateBudget.module.scss";
 
 const CreateBudget = () => {
   return (
     <div className={css.budget__overlay}>
       <div className={css.budget__card}>
-       
+        <div className={css.budget__holder}>
+          <FormControl variant="outlined">
+            <InputLabel id="custom-select-label">Choose a category</InputLabel>
+            <Select labelId="custom-select-label" id="custom-select" label="Выберите опцию">
+              <MenuItem value={10}>Опция 1</MenuItem>
+              <MenuItem value={20}>Опция 2</MenuItem>
+              <MenuItem value={30}>Опция 3</MenuItem>
+              <MenuItem value={40}>Опция 4</MenuItem>
+            </Select>
+          </FormControl>
+
+          <TextField
+            style={{
+              width: "100%",
+              maxWidth: "200px",
+              maxHeight: "49px",
+            }}
+            placeholder="Enter name"
+            multiline
+          />
+          <TextField
+            style={{
+              width: "100%",
+              maxWidth: "200px",
+              maxHeight: "49px",
+            }}
+            type="number"
+            variant="outlined"
+            label="number"
+          />
+        </div>
       </div>
     </div>
   );
